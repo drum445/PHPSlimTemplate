2fbfe31137d5370fb09d20d59c553779d706a17e
drum445 <humdrum375@gmail.com>
2018-10-06 20:28:01 +0100

    ...

--- config/routes.php
+++ config/routes.php
@@ -6,5 +6,5 @@ $app->post('/person', 'PersonController:login');
 $app->get('/person', 'PersonController:check');
 $app->delete('/person', 'PersonController:logout');
 
-$app->get('/customer/get', 'CustomerController:getAll');
+$app->get('/customer', 'CustomerController:getAll');
 
